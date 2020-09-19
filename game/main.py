b576e9b502474d427f11ae3df522ce52f64391af
pracowniaPK <kaczory5@gmail.com>
2020-09-19 18:57:40 +0200

    add websocket connection

--- game/main.py
+++ game/main.py
@@ -1,3 +1,5 @@
+import os
+
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
@@ -5,7 +7,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        print('dupa')
+        print('dupa           ')
         # self.write("Hello, world")
         self.render("template.html", title="My title", items=['hello!', '2nd', '3'])
 
@@ -14,16 +16,22 @@ class TestSocketHandler(tornado.websocket.WebSocketHandler):
         print("WebSocket opened")
 
     def on_message(self, message):
+        print('recieved: ' + message)
         self.write_message(u"You said: " + message)
 
     def on_close(self):
         print("WebSocket closed")
 
+    def check_origin(self, origin):
+        return True
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/ws", TestSocketHandler),
-    ], autoreload=True)
+        (r"/ws/", TestSocketHandler),
+        (r"/static/", tornado.web.StaticFileHandler, {'path': 'static_files'}),
+    ], autoreload=True,
+    static_path='static')
 
 if __name__ == "__main__":
     print('(re)starting')
