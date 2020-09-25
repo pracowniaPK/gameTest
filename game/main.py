451dbf9376b00397d86d0da2b2769ed60a86eb24
pracowniaPK <kaczory5@gmail.com>
2020-09-25 20:52:41 +0200

    more connections and format html

--- game/main.py
+++ game/main.py
@@ -1,7 +1,9 @@
 import os
 import time
+import random
 
 import redis
+import tornado.gen
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
@@ -29,14 +31,23 @@ class MainHandler(tornado.web.RequestHandler):
 
 class TestSocketHandler(tornado.websocket.WebSocketHandler):
     async def open(self):
-        print("WebSocket opened")
+        self.session_id = random.randint(1,1000)
+        print(f'{self.session_id}>> WebSocket opened')
+
+        async def my_loop():
+            while True:
+                await tornado.gen.sleep(5)
+                print(f'{self.session_id}>> i\'still alive')
+
+        tornado.ioloop.IOLoop.current().spawn_callback(my_loop)
+        print(f'{self.session_id}>> open end')
 
     def on_message(self, message):
-        print('recieved: ' + message)
-        self.write_message(u"You said: " + message)
+        print(f'{self.session_id}>> recieved: ' + message)
+        self.write_message(u'You said: ' + message)
 
     def on_close(self):
-        print("WebSocket closed")
+        print(f'{self.session_id}>> WebSocket closed')
 
     def check_origin(self, origin):
         return True
